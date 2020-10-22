9381280e667b7e87c468eb6372768f11c349b96c
Joe Mazzone <jmazzone@daviestech.org>
2020-10-21 21:52:21 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Scanner scan  = new Scanner(System.in);
-    
-    //Practice Using String Functions
-    String example = new String("variable");
-  }
-}
