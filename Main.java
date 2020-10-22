56df7b814dbefe38abbee6efb250289e11ae449d
Joe Mazzone <jmazzone@daviestech.org>
2020-10-22 01:49:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    Scanner scan  = new Scanner(System.in);
+    
+    //Practice Using String Functions
+    String example = new String("variable");
+  }
+}
